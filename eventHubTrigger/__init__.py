7c6b3cd74923d68a0a578ee862eba5293ddda1ac
Timibreez <ffftimi@gmail.com>
2022-07-26 02:11:32 +0100

    set logging for EventHub

--- eventHubTrigger/__init__.py
+++ eventHubTrigger/__init__.py
@@ -4,21 +4,24 @@ import azure.functions as func
 
 
 def main(event: func.EventHubEvent):
-    logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
+    body = event.get_body().decode()
+    logging.info(f"Function triggered to process a message: {body}")
+    logging.info(f"  EnqueuedTimeUtc = {event.enqueued_time}")
+    logging.info(f"  SequenceNumber = {event.sequence_number}")
+    logging.info(f"  Offset = {event.offset}")
 
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
+    result = json.loads(body)
+
+    # result = json.dumps({
+    #     'id': event.id,
+    #     'data': event.get_json(),
+    #     'topic': event.topic,
+    #     'subject': event.subject,
+    #     'event_type': event.event_type,
+    # })
 
 
-    logging.info('Python EventGrid trigger processed an event: %s', result)
+    logging.info("Python EventGrid trigger processed an event: {}".format(result))
 
 
 
