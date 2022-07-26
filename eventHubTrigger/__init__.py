d5285f972b68defe001fc6bf6b48e073e5d831ab
Timibreez <ffftimi@gmail.com>
2022-07-26 01:47:16 +0100

    setup eventHubTrigger

--- eventHubTrigger/__init__.py
+++ eventHubTrigger/__init__.py
@@ -3,7 +3,7 @@ import logging
 import azure.functions as func
 
 
-def main(event: func.EventGridEvent):
+def main(event: func.EventHubEvent):
     logging.info('Function triggered to process a message: ', event.get_body())
     logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
     logging.info('  SequenceNumber =', event.sequence_number)
